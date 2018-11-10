f2689522ef7a4c6634d63351f75c232d6b1b9d6d
bob.fan <bob.fan@centling.com>
2018-11-10 08:19:41 +0800

    fix bug

--- src/emqx_kafka_bridge.erl
+++ src/emqx_kafka_bridge.erl
@@ -23,8 +23,8 @@
 -include_lib("emqx/include/emqx.hrl").
 
 
--import(string,[concat/2]).
--import(lists,[nth/2]). 
+-import(string, [concat/2]).
+-import(lists, [nth/2]).
 
 -export([load/1, unload/0]).
 
@@ -41,7 +41,7 @@
 
 %% Called when the plugin application start
 load(Env) ->
-	ekaf_init([Env]),
+    ekaf_init([Env]),
     emqx:hook('client.connected', fun ?MODULE:on_client_connected/4, [Env]),
     emqx:hook('client.disconnected', fun ?MODULE:on_client_disconnected/3, [Env]),
     emqx:hook('message.publish', fun ?MODULE:on_message_publish/2, [Env]),
@@ -54,7 +54,7 @@ on_client_connected(#{client_id := ClientId, username := Username}, _ConnAck, _C
 
     Action = <<"connected">>,
     Now = erlang:now(),
-    Payload = [{action, Action},{device_id, ClientId}, {username, Username}, {ts, emqx_time:now_secs(Now)}],
+    Payload = [{action, Action}, {device_id, ClientId}, {username, Username}, {ts, emqx_time:now_secs(Now)}],
     %{ok, Event} = format_event(Payload),
     produce_kafka_payload(Payload),
     ok.
@@ -92,49 +92,51 @@ ekaf_init(_Env) ->
     {ok, BrokerValues} = application:get_env(emqx_kafka_bridge, broker),
     KafkaHost = proplists:get_value(host, BrokerValues),
     KafkaPort = proplists:get_value(port, BrokerValues),
-    KafkaPartitionStrategy= proplists:get_value(partitionstrategy, BrokerValues),
-    KafkaPartitionWorkers= proplists:get_value(partitionworkers, BrokerValues),
-    application:set_env(ekaf, ekaf_bootstrap_broker,  {KafkaHost, list_to_integer(KafkaPort)}),
-    % application:set_env(ekaf, ekaf_partition_strategy, KafkaPartitionStrategy),
-    application:set_env(ekaf, ekaf_partition_strategy, strict_round_robin),
-    % io:format("KafkaPartitionStrategy : ~p~n", [KafkaPartitionStrategy]),
-    % application:set_env(ekaf, ekaf_per_partition_workers, KafkaPartitionWorkers),
-    application:set_env(ekaf, ekaf_per_partition_workers, 1),
-    % io:format("KafkaPartitionWorkers : ~p~n", [KafkaPartitionWorkers]),
-    application:set_env(ekaf, ekaf_buffer_ttl, 10),
-    application:set_env(ekaf, ekaf_max_downtime_buffer_size, 5),
+    KafkaPartitionStrategy = proplists:get_value(partitionstrategy, BrokerValues),
+    KafkaPartitionWorkers = proplists:get_value(partitionworkers, BrokerValues),
+    KafkaTopic = proplists:get_value(payloadtopic, BrokerValues),
+    application:set_env(ekaf, ekaf_bootstrap_broker, {KafkaHost, list_to_integer(KafkaPort)}),
+    application:set_env(ekaf, ekaf_partition_strategy, list_to_atom(KafkaPartitionStrategy)),
+    application:set_env(ekaf, ekaf_per_partition_workers, KafkaPartitionWorkers),
+    application:set_env(ekaf, ekaf_bootstrap_topics, list_to_binary(KafkaTopic)),
+    % application:set_env(ekaf, ekaf_buffer_ttl, 100),
+    % application:set_env(ekaf, ekaf_max_downtime_buffer_size, 5),
     % {ok, _} = application:ensure_all_started(kafkamocker),
     {ok, _} = application:ensure_all_started(gproc),
-    % {ok, _} = application:ensure_all_started(ranch),    
+    % {ok, _} = application:ensure_all_started(ranch),
     {ok, _} = application:ensure_all_started(ekaf).
+
+ekaf_get_topic() ->
+    Topic = application:get_env(ekaf, ekaf_bootstrap_topics),
+    Topic.
 
 
 format_payload(Message) ->
     Username = emqx_message:get_header(username, Message),
-    
+
     Topic = Message#message.topic,
-    Tail = string:right(binary_to_list(Topic),4),
-    RawType = string:equal(Tail,<<"_raw">>),
+    Tail = string:right(binary_to_list(Topic), 4),
+    RawType = string:equal(Tail, <<"_raw">>),
     % io:format("Tail= ~s , RawType= ~s~n",[Tail,RawType]),
-    
-    MsgPayload  = Message#message.payload,
+
+    MsgPayload = Message#message.payload,
     % io:format("MsgPayload : ~s~n", [MsgPayload]),
 
     if
         RawType == true ->
             MsgPayload64 = list_to_binary(base64:encode_to_string(MsgPayload));
-            % io:format("MsgPayload64 : ~s~n", [MsgPayload64]);
+    % io:format("MsgPayload64 : ~s~n", [MsgPayload64]);
         RawType == false ->
             MsgPayload64 = MsgPayload
     end,
-    
+
 
     Payload = [{action, message_publish},
-                  {device_id, Message#message.from},
-                  {username, Username},
-                  {topic, Topic},
-                  {payload, MsgPayload64},
-                  {ts, emqx_time:now_secs(Message#message.timestamp)}],
+        {device_id, Message#message.from},
+        {username, Username},
+        {topic, Topic},
+        {payload, MsgPayload64},
+        {ts, emqx_time:now_secs(Message#message.timestamp)}],
     {ok, Payload}.
 
 
@@ -151,18 +153,10 @@ unload() ->
     emqx:unhook('message.acked', fun ?MODULE:on_message_acked/3).
 
 produce_kafka_payload(Message) ->
-    Topic = <<"Processing">>,
-    
-    {ok,MessageBody} = emqx_json:safe_encode(Message),
-    
+    Topic = ekaf_get_topic(),
+    {ok, MessageBody} = emqx_json:safe_encode(Message),
+
     % MessageBody64 = base64:encode_to_string(MessageBody),
     Payload = iolist_to_binary(MessageBody),
-
     ekaf:produce_async_batched(Topic, Payload).
-
-    % ekaf:produce_async(Topic, Payload).
-	  % io:format("send to kafka payload topic: ~s, data: ~s~n", [Topic, Payload]),
-	  % {ok, KafkaValue} = application:get_env(emq_kafka_bridge, broker),
-	  % Topic = proplists:get_value(payloadtopic, KafkaValue),
-    % lager:debug("send to kafka payload topic: ~s, data: ~s~n", [Topic, Message]),
     
